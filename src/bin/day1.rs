751b9f0340105c3d5043170820cbebd222f9e7de
iliana etaoin <iliana@buttslol.net>
2021-11-30 21:25:15 -0800

    day 1 complete

---
+++ src/bin/day1.rs
@@ -0,0 +1,22 @@
+#![no_std]
+
+use aoc2021::*;
+
+fn part1(data: impl Iterator<Item = u16>) -> usize {
+    data.tuple_windows().filter(|(a, b)| a < b).count()
+}
+
+fn part2(data: impl Iterator<Item = u16>) -> usize {
+    data.tuple_windows().filter(|(a, _, _, d)| a < d).count()
+}
+
+#[test]
+fn test() {
+    assert_eq!(part1(input!(u16)), 7);
+    assert_eq!(part2(input!(u16)), 5);
+}
+
+fn main() {
+    libc_println!("part 1: {}", part1(input!(u16)));
+    libc_println!("part 2: {}", part2(input!(u16)));
+}
---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+pub use itertools::Itertools;
+pub use libc_print::*;
+
+#[macro_export]
+macro_rules! input {
+    (@__prefix $prefix:expr) => {
+        include_str!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/input/",
+            $prefix,
+            env!("CARGO_BIN_NAME"),
+            ".txt",
+        ))
+    };
+
+    () => {{
+        #[cfg(test)] { input!(@__prefix "test_") }
+        #[cfg(not(test))] { input!(@__prefix "") }
+    }};
+
+    ($ty:ty) => {{
+        use core::str::FromStr;
+        input!().lines().map(|l| <$ty>::from_str(l).unwrap())
+    }};
+
+    ($ty:ty, $split:expr) => {
+        input!().split($split).map(|l| <$ty>::from_str(l).unwrap())
+    };
+}
